b5e949ec181a2f89c88fb1676cd9adc0feaf785d
Rich <rich@posert.com>
2023-03-27 13:30:38 -0700

    Minor changes to 3dfsc plot

--- plot_3dfsc.R
+++ plot_3dfsc.R
@@ -7,17 +7,29 @@ min_val = min(directional)
 max_val = max(directional)
 
 min_max_points = tibble(x = c(min_val, max_val), y = c(0, 0), shape = c(25, 24))
+all_points = tibble(
+  x = directional$DirectionalHist,
+  y = 0,
+  yend = -2 * 0.0075
+)
+
+histogram_binwidth = 0.0075
 
 data |> 
   ggplot() +
   theme_minimal() +
   geom_histogram(
     aes(DirectionalHist, after_stat(count) / (100 * length(directional))),
-    binwidth = 0.005,
+    binwidth = histogram_binwidth,
     fill = '#CCCCCC'
   ) +
-  geom_line(aes(GlobalX, GlobalY)) +
-  geom_point(data = min_max_points, aes(x, y, shape = shape)) +
+  geom_line(aes(GlobalX, GlobalY), size = 1) +
+  geom_segment(
+    data = all_points,
+    aes(x = x, y = y, xend = x, yend = yend),
+    alpha = 0.3,
+    position = position_jitter(width = histogram_binwidth/7, height = 0)
+  ) +
   scale_x_continuous(
     breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6),
     labels = c('DC', '10', '5', '3.3', '2.5', '2', '1.2')
@@ -33,3 +45,5 @@ data |>
     x = 'Resolution (\u00C5)',
     y = 'Global FSC'
   )
+ggsave('processed_3dfsc.pdf', width = 8, height = 4)
+ggsave('processed_3dfsc.png', width = 8, height = 4, bg = 'white')
