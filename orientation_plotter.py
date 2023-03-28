64b0220905e2f6e44ed47bc450716853f8b20446
Rich <rich@posert.com>
2023-03-28 09:23:08 -0700

    Update requirements

--- orientation_plotter.py
+++ orientation_plotter.py
@@ -4,12 +4,12 @@ import subprocess
 import shutil
 
 if len(sys.argv) != 3:
-    print('Usage: orientation_plotter.py {starfile name} {output base name}')
+    print('Usage: orientation_plotter.py {starfile name}')
     sys.exit(1)
 
 df = starfile.read(sys.argv[1])
 df = df['particles'][['rlnCoordinateX', 'rlnCoordinateY', 'rlnAngleRot', 'rlnAngleTilt', 'rlnAnglePsi']]
 df.to_csv('processed-star.csv', index = False)
 subprocess.run(['Rscript', 'plot-particles.R'])
-shutil.move('processed-star.csv', f'{sys.argv[2]}_orientations.csv')
-shutil.move('hexplot.pdf', f'{sys.argv[2]}_plot.pdf')
+shutil.move('processed-star.csv', sys.argv[1].replace(".star", "_orientations.csv"))
+shutil.move('hexplot.pdf', sys.argv[1].replace(".star", "_plot.pdf"))
